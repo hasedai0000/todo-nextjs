0204bb39c35246d09eb1c2eb53ce1118f3e6458f
hasedai0000 <91396539+hasedai0000@users.noreply.github.com>
2025-01-14 22:19:35 +0900

    todo一覧機能の実装 (#2)

---
+++ src/components/TodoList.js
@@ -0,0 +1,26 @@
+'use client'
+import useSWR from 'swr'
+import axios from '@/lib/axios'
+
+const TodoList = () => {
+    const { data: todos, error } = useSWR('/api/todos', () =>
+        axios
+            .get('/api/todos')
+            .then(res => res.data)
+            .catch(error => {
+                console.error(error)
+            }),
+    )
+
+    if (error) return 'An error has occurred.'
+
+    return (
+        <ul>
+            {todos?.map(todo => (
+                <li key={todo.id}>{todo.name}</li>
+            ))}
+        </ul>
+    )
+}
+
+export default TodoList
